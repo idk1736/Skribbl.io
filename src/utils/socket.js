4fef9ca89f32f2160ef5a74da072a826811be632
idk1736 <149055284+idk1736@users.noreply.github.com>
2025-11-06 14:37:21 +0000

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,9 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-export const socket = io('https://82839154-2e2c-4504-b192-b6f524f136c9-00-v9foiltu12xi.picard.replit.dev'); 
+const SERVER_URL = "https://82839154-2e2c-4504-b192-b6f524f136c9-00-v9foiltu12xi.picard.replit.dev";
 
-export const initSocket = () => io(SERVER_URL);
+export const socket = io(SERVER_URL, {
+  transports: ["websocket"], // Force WebSocket for reliability
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
